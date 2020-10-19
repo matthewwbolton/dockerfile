d7fb3f3d377a85167ee37a20f1b98c37c61b0e1d
Matthew Bolton <matthewwbolton@gmail.com>
2020-10-19 16:15:01 -0400

    initial commit - setup and configuration

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http
+  .createServer(function (req, res) {
+    console.log("request received");
+    res.end("omg hi", "utf-8");
+  })
+  .listen(3000);
+console.log("server started");
